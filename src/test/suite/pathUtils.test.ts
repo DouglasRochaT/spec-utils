370c0e51cda2ce775db0da79c4ae8f96d7628bdc
Douglas da Rocha <douglastuchtenhagen@gmail.com>
2022-06-20 21:15:11 -0300

    Allow to switch between specs

---
+++ src/test/suite/pathUtils.test.ts
@@ -0,0 +1,71 @@
+import * as assert from 'assert';
+import { after } from 'mocha';
+import * as path from 'path';
+import * as vscode from 'vscode';
+import { switchFile } from '../../pathUtils';
+
+const fixturesFolder = '/../../../src/test/fixtures/';
+
+suite('switchFile Test Suite', () => {
+
+  after(() => {
+    vscode.commands.executeCommand('workbench.action.closeAllEditors');
+  });
+
+  test('JavaScript Files', async () => {
+    const srcFile = path.join(__dirname + fixturesFolder + 'src/javascriptFile.js');
+    const specFile = path.join(__dirname + fixturesFolder + 'src/javascriptFile.spec.js');
+    let document = await vscode.workspace.openTextDocument(srcFile);
+
+    await vscode.window.showTextDocument(document);
+    await switchFile();
+
+    assert.strictEqual(vscode.window.activeTextEditor?.document.fileName, specFile);
+
+    await vscode.commands.executeCommand('workbench.action.closeAllEditors');
+
+    document = await vscode.workspace.openTextDocument(specFile);
+    await vscode.window.showTextDocument(document);
+    await switchFile();
+
+    assert.strictEqual(vscode.window.activeTextEditor?.document.fileName, srcFile);
+  });
+
+  test('TypeScript Files', async () => {
+    const srcFile = path.join(__dirname + fixturesFolder + 'src/typescriptFile.ts');
+    const specFile = path.join(__dirname + fixturesFolder + 'src/typescriptFile.spec.ts');
+    let document = await vscode.workspace.openTextDocument(srcFile);
+
+    await vscode.window.showTextDocument(document);
+    await switchFile();
+
+    assert.strictEqual(vscode.window.activeTextEditor?.document.fileName, specFile);
+
+    await vscode.commands.executeCommand('workbench.action.closeAllEditors');
+
+    document = await vscode.workspace.openTextDocument(specFile);
+    await vscode.window.showTextDocument(document);
+    await switchFile();
+
+    assert.strictEqual(vscode.window.activeTextEditor?.document.fileName, srcFile);
+  });
+
+  test('Ruby Files', async() => {
+    const srcFile = path.join(__dirname + fixturesFolder + 'app/ruby_file.rb');
+    const specFile = path.join(__dirname + fixturesFolder + 'spec/ruby_file_spec.rb');
+    let document = await vscode.workspace.openTextDocument(srcFile);
+
+    await vscode.window.showTextDocument(document);
+    await switchFile();
+
+    assert.strictEqual(vscode.window.activeTextEditor?.document.fileName, specFile);
+
+    await vscode.commands.executeCommand('workbench.action.closeAllEditors');
+
+    document = await vscode.workspace.openTextDocument(specFile);
+    await vscode.window.showTextDocument(document);
+    await switchFile();
+
+    assert.strictEqual(vscode.window.activeTextEditor?.document.fileName, srcFile);
+  });
+});
